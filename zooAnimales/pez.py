71af7f901d8fa5db7db2e280bcffa66b14e8e91a
SantiagoOchoaC <159485469+SantiagoOchoaC@users.noreply.github.com>
2024-03-16 22:40:36 -0500

    Clase Pez

---
+++ zooAnimales/pez.py
@@ -0,0 +1,57 @@
+from zooAnimales.animal import Animal
+
+class Pez(Animal):
+    _listado = []
+    salmones = 0 
+    bacalaos = 0 
+    
+    # Constructor
+    def __init__(self, nombre, edad, habitat, genero, colorEscamas, cantidadAletas):
+        super().__init__(nombre, edad, habitat, genero)
+        self._colorEscamas = colorEscamas
+        self._cantidadAletas = cantidadAletas
+        Pez._listado.append(self)
+
+    # Métodos 
+    @classmethod
+    def cantidadPeces(cls):
+        return len(cls._listado)
+    
+    @classmethod
+    def movimiento(cls):
+        return "nadar"
+
+    @classmethod   
+    def crearSalmon(cls, nombre, edad, genero):
+        salmon = Pez(nombre, edad, "oceano", genero, "rojo", 6)
+        cls.salmones += 1
+        return salmon
+
+    @classmethod
+    def crearBacalao(cls, nombre, edad, genero):
+        bacalao = Pez(nombre, edad, "oceano", genero, "gris", 6)
+        cls.bacalaos +=1
+        return bacalao
+
+    # Métodos get y set 
+    @classmethod
+    def getListado(cls):
+        return cls._listado
+
+    @classmethod
+    def setListado(cls, listado):
+        cls._listado = listado
+    
+    # Métodos get y set para _colorEscamas
+    def getColorEscamas(self):
+        return self._colorEscamas
+
+    def setColorEscamas(self, colorEscamas):
+        self._colorEscamas = colorEscamas
+
+    # Métodos get y set para _cantidadAletas
+    def getCantidadAletas(self):
+        return self._cantidadAletas
+
+    def setCantidadAletas(self, cantidadAletas):
+        self._cantidadAletas = cantidadAletas
