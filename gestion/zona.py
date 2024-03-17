f445a1f68c510f804b6b346d9d7f950f45867444
SantiagoOchoaC <159485469+SantiagoOchoaC@users.noreply.github.com>
2024-03-16 22:35:10 -0500

    Clase Zona

---
+++ gestion/zona.py
@@ -0,0 +1,38 @@
+class Zona():
+    def __init__(self, nombre, zoo):
+        self._nombre = nombre
+        self._zoo = zoo
+        self._animales = []
+
+    # Metodos ´
+        
+    def agregarAnimales(self, animal):
+        self._animales.append(animal)
+        animal.setZona(self)
+        
+
+    def cantidadAnimales(self):
+        return len(self._animales)
+
+    # Métodos get y set
+
+    # Métodos get y set para _nombre
+    def getNombre(self):
+        return self._nombre
+
+    def setNombre(self, nombre):
+        self._nombre = nombre
+
+    # Métodos get y set para _zoo
+    def getZoo(self):
+        return self._zoo
+
+    def setZoo(self, zoo):
+        self._zoo = zoo
+
+    # Métodos get y set para _animales
+    def getAnimales(self):
+        return self._animales
+
+    def setAnimales(self, animales):
+        self._animales = animales
